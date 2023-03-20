ceda09513209717b08677cc09d640d65460e364c
Artem K <a.kravtsov@mish.design>
2023-03-20 12:52:15 +0500

    initional Component

---
+++ Component.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const Component = () => {
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getProductsFromApi = async () => setProducts(await api("/products"));
+
+    getProductsFromApi();
+  }, []);
+
+  const addToCart = (product: { addedToCart: boolean }): void => {
+    api("/add-to-card", { product });
+
+    product.addedToCart = true;
+  };
+
+  return products.map((product) => {
+    <div className="product">
+      <div className="header">
+        <p>
+          {product.name} <small>{product.createdAt.toLocaleString()}</small>
+        </p>
+        <button onClick={() => addToCart(product)} type="button">
+          {product.addedToCart ? "Buy again" : "Buy"}
+        </button>
+      </div>
+      <div className="body">
+        <p>{product.description}</p>
+        <p>{product.attributes}</p>
+        {product.images.map((url) => (
+          <img src={url} alt={product.name} />
+        ))}
+      </div>
+      <div className="footer">
+        <p>{product.shortDescription}</p>
+        {product.price}
+        <button onClick={() => addToCart(product)} type="button">
+          {product.addedToCart ? "Buy again" : "Buy"}
+        </button>
+      </div>
+    </div>;
+  });
+};
